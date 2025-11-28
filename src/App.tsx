241149e116e85c32176f257e69e92ec1a1562274
frazermacrobert <frazermacrobert@gmail.com>
2025-11-28 12:02:45 +0000

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import React, { useEffect, useMemo, useState } from "react";
+
+const App: React.FC = () => {
+  return (
+    <div style={{ color: "white", padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Dispatch: Agency Edition</h1>
+      <p>This is the placeholder App.tsx. Gameplay logic will be added later.</p>
+    </div>
+  );
+};
+
+export default App;
