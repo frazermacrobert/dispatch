b05eca8aff5bbfdd28e07c337ee9c33d40ad47b6
frazermacrobert <frazermacrobert@gmail.com>
2025-11-28 13:16:08 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/dispatch/",
   plugins: [react()],
-  server: { port: 5173 },
-  build: {
-    outDir: 'docs'
-  }
+  base: "/dispatch/",    // tell Vite your repo subpath
+  server: { port: 5173 }
 });
